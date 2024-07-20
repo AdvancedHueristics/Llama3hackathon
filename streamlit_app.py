27f0e0808f064f69b511dfa786ab7208855ea56d
owais <owaisahmed142002@gmail.com>
2024-07-20 21:36:21 +0500

    Resolved bugs regarding cache and did some enhancements for irrelevant queries

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,18 @@ import pandas as pd
 from pandasai import Agent
 from langchain_groq import ChatGroq
 from pandasai.responses.streamlit_response import StreamlitResponse
-import io
+import io, json
 from PIL import Image
+from together import Together
+
+
+def get_env_data_as_dict(path: str) -> dict:
+    with open(path, 'r') as f:
+       return dict(tuple(line.replace('\n', '').split('=')) for line
+                in f.readlines() if not line.startswith('#'))
+
+env_dict = get_env_data_as_dict('.env')
+client = Together(api_key=env_dict.get("TOGETHER_API_KEY", ""))
 
 def get_env_data_as_dict(path: str) -> dict:
     with open(path, 'r') as f:
@@ -13,6 +23,9 @@ def get_env_data_as_dict(path: str) -> dict:
 # Load environment variables
 env_dict = get_env_data_as_dict('.env')
 model = ChatGroq(model_name='llama3-70b-8192', api_key=env_dict.get("GROQ_API_KEY", ""))
+
+SYSTEM_PROMPT_TOGETHER = '''You are an AI Query analyzer. Analyze given user query that whether it is related to any CSV, data or charts. If yes then return {"status":True} else then make a reply for the query as polite and helpful member and also state "I am a Data Wizard and I can only help you with Data Science task" return response example as {"status":false, "message":"How are you?"}. If the user query contains Hi or greeting phrases then dont return status as False with relevant reply. The data contains following columns '''
+
 
 st.title("DataWizard: Your DataScience Assistant")
 
@@ -29,8 +42,20 @@ def generate_response():
         st.session_state.chat_history.insert(0, {"role": "user", "text": prompt})
         with st.spinner("Generating response..."):
             try:
-                response = agent.chat(prompt)
-                # Handle the response based on its type
+                is_question_relevant = client.chat.completions.create(
+                    model="mistralai/Mixtral-8x7B-Instruct-v0.1",
+                    messages=[
+                        {"role": "system", "content": SYSTEM_PROMPT_TOGETHER + str(list(data.columns))},
+                        {"role": "user", "content": prompt}
+                        ],
+                    response_format ={"type": "json_object"}
+                    )
+                is_question_relevant = json.loads(is_question_relevant.choices[0].message.content)
+                if not is_question_relevant['status']:
+                    response = is_question_relevant['message']
+                else:
+                    response = agent.chat(prompt)
+
                 if isinstance(response, str):
                     if response.endswith(".png"):
                         img_buf = io.BytesIO()
@@ -64,6 +89,7 @@ if uploaded_file is not None:
         # Input field for user prompt
         # Input field for user prompt
         prompt = st.text_input("Ask questions about your data:", value="", key="prompt_input", on_change=None)
+
         # Generate button with callback
         st.button("Generate", on_click=generate_response)
         # Display chat history with the latest message at the top
