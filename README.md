0025d1e213893656539fe6f2ba7dbd3e6b3f1288
Hasnain Ali <beinghasnain16@gmail.com>
2024-07-20 18:36:47 +0500

    updates

--- README.md
+++ README.md
@@ -1,11 +1,54 @@
-# LLAMA 3 Hackathon
+# DataWizard: Your DataScienceAssistant
 
-### Installation Steps
-create a virtual env
-`conda create --name wizard`
+Welcome to **DataWizard**, your go-to assistant for all things data science! This project leverages the power of **Llama 3** through Together AI and Groq APIs to assist users in their data science tasks with ease. DataWizard employs the pandasai agent for data manipulation and analysis and offers an intuitive UI developed in **Streamlit**.
 
-activate it
-`conda activate wizard`
+## Features
 
-install requirements
-pip install -r requirements.txt
+- **Llama 3 Integration**: Utilize the state-of-the-art Llama 3 model for advanced data science tasks.
+- **Together AI and Groq APIs**: Seamlessly interact with these APIs for efficient processing.
+- **pandasai Agent**: Enhance your data manipulation and analysis capabilities.
+- **Streamlit UI**: Enjoy a user-friendly interface for an interactive experience.
+
+## LLAMA 3 Hackathon
+
+This project is part of the **LLAMA 3 Hackathon** organized by **lablabai**.
+
+## Installation Steps
+
+Follow these steps to set up and run DataWizard on your local machine.
+
+1. **Create a virtual environment**:
+    ```bash
+    conda create --name wizard
+    ```
+
+2. **Activate the virtual environment**:
+    ```bash
+    conda activate wizard
+    ```
+
+3. **Install the required packages**:
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+4. **Run the application**:
+    ```bash
+    streamlit run streamlit_app.py
+    ```
+
+## Usage
+
+Once the application is running, navigate to the provided local URL to start using DataWizard. Explore various data science functionalities and make the most out of the Llama 3 integration!
+
+## Contributing
+
+We welcome contributions from the community. Feel free to submit pull requests or report issues.
+
+## License
+
+This project is licensed under the MIT License.
+
+---
+
+Enjoy using DataWizard and make your data science tasks easier and more efficient!
--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,11 @@ import streamlit as st
 import pandas as pd
 from pandasai import Agent
 from langchain_groq import ChatGroq
+import matplotlib
 import matplotlib.pyplot as plt
+from pandasai.responses.streamlit_response import StreamlitResponse
 import io
+from PIL import Image
 
 def get_env_data_as_dict(path: str) -> dict:
     with open(path, 'r') as f:
@@ -22,40 +25,54 @@ if 'chat_history' not in st.session_state:
 if 'prompt_input' not in st.session_state:
     st.session_state.prompt_input = ""
 
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = False
+
+def click_button():
+    st.session_state.clicked = True
+    st.rerun()
+
 # File uploader in sidebar
 uploaded_file = st.sidebar.file_uploader("Upload a CSV file", type=["csv"])
+
+def call_agent():
+    response = agent.chat(prompt)
+    print(response)
+    # Handle the response based on its type
+    if isinstance(response, str):
+        if response.endswith(".png"):
+            # Save the plot to a BytesIO object
+            img_buf = io.BytesIO()
+            image = Image.open(response)
+            image.save(img_buf, format='PNG')
+            img_buf.seek(0)
+            st.session_state.chat_history.insert(0, {"role": "assistant", "image": img_buf})
+        else:
+            st.session_state.chat_history.insert(0, {"role": "assistant", "text": response})
+
+    else:
+        st.session_state.chat_history.insert(0, {"role": "assistant", "text": str(response)})
 
 if uploaded_file is not None:
     data = pd.read_csv(uploaded_file)
     st.write(data.head(3))
 
-    agent = Agent(data, config={"llm": model})
+    agent = Agent(data,
+                  config={"llm": model,
+                "save_charts": True,
+                "verbose": True,
+                "response_parser": StreamlitResponse})
 
     # Container for the chat interface
     with st.expander("Chat", expanded=True):
         # Input field for user prompt
         prompt = st.text_input("Ask questions about your data:", value=st.session_state.prompt_input, key="prompt_input")
 
-        if st.button("Generate"):
+        if st.button("Generate", on_click=call_agent):
             if prompt:
                 st.session_state.chat_history.insert(0, {"role": "user", "text": prompt})
-
                 with st.spinner("Generating response..."):
-                    response = agent.chat(prompt)
-
-                    # Handle the response based on its type
-                    if isinstance(response, str):
-                        st.session_state.chat_history.insert(0, {"role": "assistant", "text": response})
-                    elif isinstance(response, pd.DataFrame):
-                        st.session_state.chat_history.insert(0, {"role": "assistant", "text": response.to_string()})
-                    elif isinstance(response, plt.Figure):
-                        # Save the plot to a BytesIO object
-                        img_buf = io.BytesIO()
-                        response.savefig(img_buf, format='png')
-                        img_buf.seek(0)
-                        st.session_state.chat_history.insert(0, {"role": "assistant", "image": img_buf})
-                    else:
-                        st.session_state.chat_history.insert(0, {"role": "assistant", "text": str(response)})
+                   call_agent()
 
                 # Clear the input field by updating session state
                 #st.session_state.prompt_input = ""
